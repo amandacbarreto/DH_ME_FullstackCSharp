de007605a6cded7c6bad912bb01d18d4fa49a65d
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-21 22:30:55 -0400

    Atualiza conteúdo da Aula 09

---
+++ 09/Atividade09/Resolucao_Atividade09/Program.cs
@@ -0,0 +1,145 @@
+﻿double resultado = 0;
+string opcaoUsuario = ObterOpcaoUsuario();
+
+while (opcaoUsuario != "0")
+{
+    switch (opcaoUsuario)
+    {
+        case "1":
+            Console.WriteLine("Você digitou 1 - Somar. Qual valor deseja somar?");
+            break;
+        case "2":
+            Console.WriteLine("Você digitou 2 - Subtrair. Qual valor deseja subtrair?");
+            break;
+        case "3":
+            Console.WriteLine("Você digitou 3 - Multiplicar. Qual valor deseja multiplicar?");
+            break;
+        case "4":
+            Console.WriteLine("Você digitou 4 - Dividir. Qual valor deseja dividir?");
+            break;
+        case "5":
+            Console.WriteLine("Você digitou 5 - Zerar calculadora.");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine(Calcular(opcaoUsuario));
+
+    opcaoUsuario = ObterOpcaoUsuario();
+}
+
+Console.WriteLine("Obrigado por utilizar nossos serviços");
+
+double Calcular (string opcao)
+{
+    try { 
+
+        return (opcao) switch
+        {
+            "1" => somarValor(validaValor(0)),
+            "2" => subtrairValor(validaValor(0)),
+            "3" => multiplicarValor(validaValor(1)),
+            "4" => dividirValor(validaValor(1)),
+            "5" => zerarResultado(),
+            _ => 0
+        };
+    }
+    catch (DivideByZeroException erro)
+    {
+        Console.WriteLine(Environment.NewLine + "CodError001 - Divisão por 0 não é possível.");
+        Console.WriteLine(erro.GetType());
+        Console.WriteLine("Localização do erro." + Environment.NewLine);
+        Console.WriteLine("Por favor, digite um numero." + Environment.NewLine);
+    }
+
+    return pegarResultadoDaUltimaOperacaoMatematica();
+}
+
+double validaValor(int valorIndiferente)
+{
+    double operando = valorIndiferente;
+    try
+    {
+        operando = Double.Parse(Console.ReadLine());
+    }
+    catch (Exception erro)
+    {
+        Console.WriteLine(Environment.NewLine + "CodError002 - O usuário não pode inserir qualquer valor além de números.");
+        Console.WriteLine(erro.GetType());
+        Console.WriteLine("Localização do erro." + Environment.NewLine);
+        Console.WriteLine("Por favor, digite um numero." + Environment.NewLine);
+    }
+    return operando;
+}
+
+double somarValor(double valor)
+{
+    double resultadoSoma = pegarResultadoDaUltimaOperacaoMatematica() + valor;
+    definirResultadoDaUltimaOperacaoMatematica(resultadoSoma);
+    return resultadoSoma;
+}
+
+
+
+double subtrairValor(double valor)
+{
+    double resultadoSubtração = pegarResultadoDaUltimaOperacaoMatematica() - valor;
+    definirResultadoDaUltimaOperacaoMatematica(resultadoSubtração);
+    return resultadoSubtração;
+}
+
+double dividirValor(double valor)
+{
+    if (valor == 0)
+    {
+        throw new DivideByZeroException("CodError001 - Divisão por 0 não é possível.");
+    }
+    else
+    {
+        double resultadoDivisao = pegarResultadoDaUltimaOperacaoMatematica() / valor;
+        definirResultadoDaUltimaOperacaoMatematica(resultadoDivisao);
+        return resultadoDivisao;
+    }
+}
+
+double multiplicarValor(double valor)
+{
+    double resultadoMultiplicação = pegarResultadoDaUltimaOperacaoMatematica() * valor;
+    definirResultadoDaUltimaOperacaoMatematica(resultadoMultiplicação);
+    return resultadoMultiplicação;
+}
+
+double zerarResultado()
+{
+    double zerar = resultado * 0;
+    definirResultadoDaUltimaOperacaoMatematica(zerar);
+    return zerar;
+}
+
+double pegarResultadoDaUltimaOperacaoMatematica()
+{
+    return resultado;
+}
+
+void definirResultadoDaUltimaOperacaoMatematica(double resultadoOperacao)
+{
+    resultado = resultadoOperacao;
+}
+
+
+string ObterOpcaoUsuario()
+{
+    Console.WriteLine("Escolha qual operação deseja realizar: ");
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Zerar calculadora");
+    Console.WriteLine("0 - Para sair da calculadora");
+
+    return Console.ReadLine();
+}
+
+
--- 09/README.md
+++ 09/README.md
@@ -1,13 +1,53 @@
-# Aula 01 - Título
+# Aula 09 (13/06/2022) - Tratamento de Erro
 
-## Conteúdo
-
-### Assunto `destaque`/`catch`/`finally`
+## ### Tratando uma Excessão com `try`/`catch`/`finally`
 
 ```cs
-
+try
+{
+    // Forçamos o erro ao tentar dividir um valor inteiro por 0.
+    int calculo = 10 / 0;
+    Console.WriteLine(calculo);
+}
+catch (Exception error) when (calculo != 10)
+{
+    Console.WriteLine(error.GetType());
+}
+catch(Exception error)
+{
+    // No bloco de tratamento do erro capturamos...
+    Console.WriteLine("Código do Error AbCd123");
+    // Tipo do Erro nesse techo do código.
+    Console.WriteLine(error.GetType());
+}
+finally
+{
+    // Definimos uma instrução que deverá ser executada após o bloco try/catch.
+    // Nota: Esse trecho de código será executado independente de existir uma excessão ou não.
+    Console.WriteLine("Reinicie o programa...");
+}
 ```
 
-## [Atividade proposta](link da mesa de trabalho)
-## [Atividade entregue](link do repositorio do exercicio desenvolvido em aula)
-## [Nova resolução](atividade individual para treino)
+> **Note**
+>
+> É possível aninhar vários Catches, deixando casos mais gerais por último.
+
+### Criando uma Excessão com `throw`
+
+```cs
+int capturaDaIdadeDoUsuario = Console.ReadLine();
+bool valorCapturadoForValido = int.TryParse(capturaDaIdadeDoUsuario, out idadeDoUsuario);
+if(valorCapturadoForValido) {
+    Console.WriteLine(idadeDoUsuario);
+}
+else {
+    // Uma Excessão é cadastrada para tratamento.
+    throw new Exception("Valor inserido é inválido.");
+}
+```
+
+---
+
+- [Repositório oficial](https://github.com/wssantanna/mercado-eletr-nico-0522cdmencn01bred/blob/main/09/README.md)
+- [Atividade entregue](https://github.com/lanyarag/calculadora)
+- [Nova resolução](./Atividade09/Resolucao_Atividade09/Program.cs)
