4101973d5a7473fafeab340e85d0e976a1622f01
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-23 12:36:19 -0300

    Acrescenta Refatoração da Aula 06

---
+++ 10/Atividade10/Refatoracao_Aula06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Refatoracao_Aula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insira o nome do usuario");
+            string nome = Console.ReadLine()!;
+
+            Console.WriteLine("Insira o ano de nascimento do usuario");
+            int.TryParse(Console.ReadLine(), out int anoNascimento);
+
+            Usuario usuario = new Usuario(nome,anoNascimento);
+            Console.WriteLine($"A idade do usuário é {usuario.CalcularIdade()}");
+            Console.WriteLine($"A idade do usuário é {usuario.Idade}");
+
+            Usuario usuario2 = new Usuario();
+            Console.WriteLine("Insira o nome do usuario 2");
+
+            usuario2.Nome = Console.ReadLine()!;
+
+            Console.WriteLine("Insira o ano de nascimento do usuario");
+            int.TryParse(Console.ReadLine(), out int anoNascimento2);
+            usuario2.AnoNascimento = anoNascimento2;
+
+            Console.WriteLine($"A idade do usuário 2 é {usuario2.CalcularIdade()}");
+            Console.WriteLine($"A idade do usuário 2 é {usuario2.Idade}");
+
+        }
+    }
+}
---
+++ 10/Atividade10/Refatoracao_Aula06/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refatoracao_Aula06
+{
+    internal class Usuario
+    {
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int AnoNascimento { get; set; }
+
+        public int AnoAtual = DateTime.Now.Year;
+
+        public Usuario(string nome, int anoNascimento)
+        {
+            Nome = nome;
+            AnoNascimento = anoNascimento;
+        }
+
+        public Usuario() { }
+
+
+
+        public int CalcularIdade()
+        {
+            Idade = AnoAtual - AnoNascimento;
+            return Idade;
+        }
+
+    }
+}
