d043b9d9572fe249752995602942fbad0f612e98
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-24 13:00:44 -0300

    Acrescenta Refatoração da Aula 09

---
+++ 10/Atividade10/Refatoracao_Aula09/Calculadora.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refatoracao_Aula09
+{
+    class Calculadora
+    {
+        private string[] Opcoes = new string[6] { "somar", "subtrair", "multiplicar", "dividir", "zerar calculadora", "sair da calculadora" }; 
+
+
+        private double HistoricoCalculadora { get; set; }
+   
+        public double Calcular (double valor, string operacao)
+        {
+            try
+            {
+
+                return operacao switch
+                {
+                    "1" => HistoricoCalculadora += valor,
+                    "2" => HistoricoCalculadora -= valor,
+                    "3" => HistoricoCalculadora *= valor,
+                    "4" => DividirValor(valor),
+                    "5" => HistoricoCalculadora = 0,
+                    _ => HistoricoCalculadora
+                };
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(Environment.NewLine + "CodError001 - Divisão por 0 não é possível.");
+                Console.WriteLine(erro.GetType());
+                Console.WriteLine("Localização do erro." + Environment.NewLine);
+                Console.WriteLine("Por favor, digite um numero." + Environment.NewLine);
+            }
+
+            return HistoricoCalculadora;
+        }
+
+        public double ValidarValor(double valorIndiferente)
+        {
+            double operando = valorIndiferente;
+            try
+            {
+                operando = Double.Parse(Console.ReadLine()!);
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(Environment.NewLine + "CodError002 - O usuário não pode inserir qualquer valor além de números.");
+                Console.WriteLine(erro.GetType());
+                Console.WriteLine("Localização do erro." + Environment.NewLine);
+                Console.WriteLine("Por favor, digite um numero." + Environment.NewLine);
+            }
+            return operando;
+        }
+
+        double DividirValor(double valor)
+        {
+            if (valor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                return HistoricoCalculadora /= valor;
+            }
+        }
+
+        public string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Escolha qual operação deseja realizar: ");
+            for (int i = 0; i < Opcoes.Length; i++)
+            {
+                Console.WriteLine($"{i+1} - {Opcoes[i].ToUpper()}");
+            }
+
+            return Console.ReadLine()!;
+        }
+    }
+}
---
+++ 10/Atividade10/Refatoracao_Aula09/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Refatoracao_Aula09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 0, elementoNeutro = 0;
+            Calculadora calculadora = new Calculadora();
+            string opcaoUsuario = calculadora.ObterOpcaoUsuario();
+
+            while (opcaoUsuario != "0")
+            {
+                if(opcaoUsuario == "1" || opcaoUsuario == "2" || opcaoUsuario == "3" || opcaoUsuario == "4" || opcaoUsuario == "5")
+                {
+                    switch (opcaoUsuario)
+                    {
+                        case "1":
+                            Console.WriteLine("Você digitou 1 - Somar. Qual valor deseja somar?");
+                            elementoNeutro = 0;
+                            break;
+                        case "2":
+                            Console.WriteLine("Você digitou 2 - Subtrair. Qual valor deseja subtrair?");
+                            elementoNeutro = 0;
+                            break;
+                        case "3":
+                            Console.WriteLine("Você digitou 3 - Multiplicar. Qual valor deseja multiplicar?");
+                            elementoNeutro = 1;
+                            break;
+                        case "4":
+                            Console.WriteLine("Você digitou 4 - Dividir. Qual valor deseja dividir?");
+                            elementoNeutro = 1;
+                            break;
+                        case "5":
+                            Console.WriteLine("Você digitou 5 - Zerar calculadora.");
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida");
+                            break;
+                    }
+                    valor = calculadora.ValidarValor(elementoNeutro);
+                    Console.WriteLine(calculadora.Calcular(valor, opcaoUsuario));
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida");
+                }
+
+                opcaoUsuario = calculadora.ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+
+        }
+    }
+}
+
